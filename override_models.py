4a20319f2ed6813972093bbc5718745308d19c5d
pappuchtri <pappuchtri@gmail.com>
2025-05-30 14:56:27 +0545

    Update override_models.py

--- override_models.py
+++ override_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Boolean, Enum
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Boolean, Enum, Float
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
